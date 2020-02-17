75a63be35a8359da28b007df3999136865987aad
Arkadiysm <45769688+Arkadiysm@users.noreply.github.com>
2020-02-17 12:05:29 +0500

    fix goodspage
    fix goodspage redirect

--- src/components/GoodsPage.js
+++ src/components/GoodsPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 
 
 const GoodsPage = (props) => {
   const {goods, match: {params}, handleAddToCart} = props;
   const {id} = params;
-  const {name, price, cover, description} = goods.filter( (i) => i.id === id )[0];
+  const item = goods.filter( (i) => i.id === id )[0];
+  if (item === undefined) return <Redirect to="/" />;
+  const {name, price, cover, description} = item;
   return (
     <>
       <div className="goods-page">
